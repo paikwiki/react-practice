042a6f1138f781617741167ef43792bc74bea257
paikwiki <paikwiki@gmail.com>
2019-01-05 22:58:23 +0900

    Error handling

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ class App extends Component {
       results: null,
       searchKey: '',
       searchTerm: DEFAULT_QUERY,
+      error: null,
     };
     
     this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
@@ -67,7 +68,7 @@ class App extends Component {
     fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
-      .catch(error => error );
+      .catch(error => this.setState({error}));
   }
 
   componentDidMount() {
@@ -108,7 +109,8 @@ class App extends Component {
     const {
       searchTerm,
       results,
-      searchKey
+      searchKey,
+      error
     } = this.state;
 
     const page = (
@@ -123,6 +125,9 @@ class App extends Component {
       results[searchKey].hits
     ) || [];
     
+    if (error) {
+      return <p>Something went wrong.</p>
+    }
 
     return (
       <div className="page">
@@ -135,10 +140,15 @@ class App extends Component {
             Search
           </Search>
         </div>
-        <Table
-          list={list}
-          onDismiss={this.onDismiss}
-        />
+        { error 
+          ? <div className="interactions">
+              <p>Something went wrong.</p>
+            </div>
+          : <Table
+              list={list}
+              onDismiss={this.onDismiss}
+            />
+        }
         <div className="interactions">
           <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
             More
